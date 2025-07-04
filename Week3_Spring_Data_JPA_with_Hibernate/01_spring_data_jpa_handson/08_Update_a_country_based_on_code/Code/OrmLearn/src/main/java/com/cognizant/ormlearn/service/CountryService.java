282768bee2dbb275ce963dc5bab4df63e1b82702
nameissakthi <svel7252@gmail.com>
2025-07-04 12:26:56 +0530

    Put Mapping

---
+++ Week3_Spring_Data_JPA_with_Hibernate/01_spring_data_jpa_handson/08_Update_a_country_based_on_code/Code/OrmLearn/src/main/java/com/cognizant/ormlearn/service/CountryService.java
@@ -0,0 +1,31 @@
+package com.cognizant.ormlearn.service;
+
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryService {
+
+    private CountryRepository countryRepository;
+
+    public CountryService( @Autowired CountryRepository countryRepository ) {
+        this.countryRepository = countryRepository;
+    }
+
+
+    public String updateCountry(String code, String name) {
+        try{
+            Country country = countryRepository.findById(code).orElse(null);
+            assert country != null;
+
+            country.setName(name);
+
+            countryRepository.save(country);
+            return  "Country Updated Successfully";
+        }catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+}
